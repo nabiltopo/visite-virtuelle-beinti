f77117aaef98f8bae227d4c9a66624c654d2d8cc
nabiltopo <63020077+nabiltopo@users.noreply.github.com>
2025-11-05 01:04:17 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,59 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_20250612162046_0001_v",
+      "name": "DJI_20250612162046_0001_V",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.34123053765849676,
+          "pitch": 0.5536792343088628,
+          "title": "Mosqué <strong style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">Beinti</strong><span style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">&nbsp;</span>",
+          "text": "Mosqué"
+        },
+        {
+          "yaw": -3.1250760901255497,
+          "pitch": 0.728432584913957,
+          "title": "Souk&nbsp;<strong style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">Beinti</strong><span style=\"color: rgb(34, 34, 34); font-family: Arial, Helvetica, sans-serif; font-size: small; background-color: rgb(255, 255, 255);\">&nbsp;</span>",
+          "text": "souk"
+        }
+      ]
+    }
+  ],
+  "name": "Visite Virtuelle Beinti ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
